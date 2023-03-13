6cfc33e679ff757cb5af9ee1338bbf0501b28a25
Ersin <112959117+ersin98@users.noreply.github.com>
2023-03-13 12:17:35 +0300

    gun2Odev2

---
+++ person.py
@@ -0,0 +1,12 @@
+class Person:
+  #  def __init__(self) -> None:
+  #      pass
+    def __init__(self,name,lasName):
+        self.name=name
+        self.lastName=lasName
+
+musteri1 = Person("Ali","Taş")
+musteri2 = Person("Veli","Toprak")
+musteri3 = Person("Selim","Duman")
+print(musteri1.name)
+#https://youtu.be/6IWpowC2ryc?list=PLqG356ExoxZWbfIhu2C7b0dKIGHKeGKcM&t=19431
